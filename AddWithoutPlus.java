ba56160299fa22785124d7653afa4d0b1c2384c2
Andreas Loizou (LACK3R) <andreasl.loizou@gmail.com>
2016-01-05 20:13:25 +0200

    Update AddWithoutPlus.java

--- AddWithoutPlus.java
+++ AddWithoutPlus.java
@@ -1,7 +1,18 @@
+
 public class AddWithoutPlus {
 
-
 	static public int addLeFast(int n1, int n2){
+		int res = (n1^n2);
+		int reminder=(n1&n2)<<1;
+		while (reminder!=0){
+			int origRes = res;
+			res = (res^reminder);
+			reminder=(origRes&reminder)<<1;
+		}
+		return res;
+	}
+
+	static public int addLeFast2(int n1, int n2){
 		int res = (n1|n2)&~(n1&n2);
 		int reminder=(n1&n2)<<1;
 		while (reminder!=0){
@@ -11,6 +22,7 @@ public class AddWithoutPlus {
 		}
 		return res;
 	}
+	
 	
 	static public int add(int n1, int n2){
 		int res = 0;
